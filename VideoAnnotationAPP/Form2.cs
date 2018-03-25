be58092e58012487269943aa1d5cd4e5ec20a58f
lychkovi <lychkovi@gmail.com>
2018-03-26 00:22:11 +0300

    Добавлены и отлажены классы для представления списка траекторий в памяти и его загрузки (сохранения) из (в) XML

--- VideoAnnotationAPP/Form2.cs
+++ VideoAnnotationAPP/Form2.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 
 using System.Runtime.InteropServices;       // для DllImport native C++
+using System.Xml;
 
 namespace VideoAnnotationAPP
 {
@@ -31,10 +32,38 @@ namespace VideoAnnotationAPP
         int ixFrameCurr;        // индекс текущего кадра видео
         Image frameCurr;          // текущий кадр видео
 
+        // Коллекция содержит траектории всех объектов на видео
+        TrackList tracks;
+
         public Form2()
         {
             InitializeComponent();
             isVideo = false;
+
+            // Инициализируем коллекцию траекторий
+            tracks = new TrackList();
+            tracks.videoFilePath = "none";
+            tracks.Add(1, new Track());
+            tracks[1].Add(1, new TrackNode());
+            tracks[1].Add(2, new TrackNode());
+            tracks.Add(2, new Track());
+            tracks[2].Add(1, new TrackNode());
+            tracks[2].Add(2, new TrackNode());
+
+            // Выполняем сериализацию списка траекторий
+            XmlTextWriter writer = new XmlTextWriter("tracks.xml", Encoding.Unicode);
+            tracks.WriteXml(writer);
+            writer.Close();
+
+            // Выполняем десериализацию списка траекторий
+            XmlTextReader reader = new XmlTextReader("tracks.xml");
+            TrackList tracks2 = new TrackList();
+            tracks2.ReadXml(reader);
+
+            // Выполняем повторную сериализацию (для контроля)
+            XmlTextWriter writer2 = new XmlTextWriter("tracks2.xml", Encoding.Unicode);
+            tracks2.WriteXml(writer2);
+            writer2.Close();
         }
 
         // Метод перематывает видео до нужной позиции
@@ -135,3 +164,129 @@ namespace VideoAnnotationAPP
 
     }
 }
+
+/* Работа со словарём
+using System;
+using System.Collections.Generic;
+
+public class Example
+{
+    public static void Main()
+    {
+        // Create a new dictionary of strings, with string keys.
+        //
+        Dictionary<string, string> openWith = 
+            new Dictionary<string, string>();
+
+        // Add some elements to the dictionary. There are no 
+        // duplicate keys, but some of the values are duplicates.
+        openWith.Add("txt", "notepad.exe");
+        openWith.Add("bmp", "paint.exe");
+        openWith.Add("dib", "paint.exe");
+        openWith.Add("rtf", "wordpad.exe");
+
+        // The Add method throws an exception if the new key is 
+        // already in the dictionary.
+        try
+        {
+            openWith.Add("txt", "winword.exe");
+        }
+        catch (ArgumentException)
+        {
+            Console.WriteLine("An element with Key = \"txt\" already exists.");
+        }
+
+        // The Item property is another name for the indexer, so you 
+        // can omit its name when accessing elements. 
+        Console.WriteLine("For key = \"rtf\", value = {0}.", 
+            openWith["rtf"]);
+
+        // The indexer can be used to change the value associated
+        // with a key.
+        openWith["rtf"] = "winword.exe";
+        Console.WriteLine("For key = \"rtf\", value = {0}.", 
+            openWith["rtf"]);
+
+        // If a key does not exist, setting the indexer for that key
+        // adds a new key/value pair.
+        openWith["doc"] = "winword.exe";
+
+        // The indexer throws an exception if the requested key is
+        // not in the dictionary.
+        try
+        {
+            Console.WriteLine("For key = \"tif\", value = {0}.", 
+                openWith["tif"]);
+        }
+        catch (KeyNotFoundException)
+        {
+            Console.WriteLine("Key = \"tif\" is not found.");
+        }
+
+        // When a program often has to try keys that turn out not to
+        // be in the dictionary, TryGetValue can be a more efficient 
+        // way to retrieve values.
+        string value = "";
+        if (openWith.TryGetValue("tif", out value))
+        {
+            Console.WriteLine("For key = \"tif\", value = {0}.", value);
+        }
+        else
+        {
+            Console.WriteLine("Key = \"tif\" is not found.");
+        }
+
+        // ContainsKey can be used to test keys before inserting 
+        // them.
+        if (!openWith.ContainsKey("ht"))
+        {
+            openWith.Add("ht", "hypertrm.exe");
+            Console.WriteLine("Value added for key = \"ht\": {0}", 
+                openWith["ht"]);
+        }
+
+        // When you use foreach to enumerate dictionary elements,
+        // the elements are retrieved as KeyValuePair objects.
+        Console.WriteLine();
+        foreach( KeyValuePair<string, string> kvp in openWith )
+        {
+            Console.WriteLine("Key = {0}, Value = {1}", 
+                kvp.Key, kvp.Value);
+        }
+
+        // To get the values alone, use the Values property.
+        Dictionary<string, string>.ValueCollection valueColl =
+            openWith.Values;
+
+        // The elements of the ValueCollection are strongly typed
+        // with the type that was specified for dictionary values.
+        Console.WriteLine();
+        foreach( string s in valueColl )
+        {
+            Console.WriteLine("Value = {0}", s);
+        }
+
+        // To get the keys alone, use the Keys property.
+        Dictionary<string, string>.KeyCollection keyColl =
+            openWith.Keys;
+
+        // The elements of the KeyCollection are strongly typed
+        // with the type that was specified for dictionary keys.
+        Console.WriteLine();
+        foreach( string s in keyColl )
+        {
+            Console.WriteLine("Key = {0}", s);
+        }
+
+        // Use the Remove method to remove a key/value pair.
+        Console.WriteLine("\nRemove(\"doc\")");
+        openWith.Remove("doc");
+
+        if (!openWith.ContainsKey("doc"))
+        {
+            Console.WriteLine("Key \"doc\" is not found.");
+        }
+    }
+}
+
+*/
---
+++ VideoAnnotationAPP/Track.cs
@@ -0,0 +1,216 @@
+﻿// Класс представляет размеченную траекторию отдельного объета на видео.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace VideoAnnotationAPP
+{
+
+    public class TrackNode : IXmlSerializable
+    {
+        int left;
+        int top;
+        int right;
+        int bottom;
+        int iframe;
+
+        #region IXmlSerializable Members
+        public System.Xml.Schema.XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            XmlSerializer intSerializer = new XmlSerializer(typeof(int));
+
+            bool wasEmpty = reader.IsEmptyElement;
+            reader.Read();
+
+            if (wasEmpty)
+                return;
+
+            {
+                reader.ReadStartElement("left");
+                left = (int)intSerializer.Deserialize(reader);
+                reader.ReadEndElement();
+
+                reader.ReadStartElement("top");
+                top = (int)intSerializer.Deserialize(reader);
+                reader.ReadEndElement();
+
+                reader.ReadStartElement("right");
+                right = (int)intSerializer.Deserialize(reader);
+                reader.ReadEndElement();
+
+                reader.ReadStartElement("bottom");
+                bottom = (int)intSerializer.Deserialize(reader);
+                reader.ReadEndElement();
+            }
+            reader.ReadEndElement();
+        }
+
+        public void WriteXml(System.Xml.XmlWriter writer)
+        {
+            XmlSerializer intSerializer = new XmlSerializer(typeof(int));
+
+            //writer.WriteStartElement("TrackNode");
+
+            writer.WriteStartElement("left");
+            intSerializer.Serialize(writer, left);
+            writer.WriteEndElement();
+
+            writer.WriteStartElement("top");
+            intSerializer.Serialize(writer, top);
+            writer.WriteEndElement();
+
+            writer.WriteStartElement("right");
+            intSerializer.Serialize(writer, right);
+            writer.WriteEndElement();
+
+            writer.WriteStartElement("bottom");
+            intSerializer.Serialize(writer, bottom);
+            writer.WriteEndElement();
+
+            //writer.WriteEndElement();
+        }
+        #endregion
+    }
+
+    public class Track : SerializableDictionary<int, TrackNode>, IXmlSerializable
+    {
+        int objType;            // тип движущегося объекта
+
+        protected override string listName
+        {
+            get { return "NodeList"; }
+        } 
+
+        protected override string itemName
+        {
+            get { return "NodeListItem"; }
+        }
+
+        protected override string keyName
+        {
+            get { return "FrameNumber"; }
+        }
+
+        protected override string valueName
+        {
+            get { return "Value"; }
+        }
+
+        #region IXmlSerializable Members
+
+        public new void ReadXml(System.Xml.XmlReader reader)
+        {
+            XmlSerializer intSerializer = new XmlSerializer(typeof(int));
+
+            bool wasEmpty = reader.IsEmptyElement;
+            reader.Read();
+
+            if (wasEmpty)
+                return;
+
+            {
+                reader.ReadStartElement(listName);
+
+                reader.ReadStartElement("objType");
+                objType = (int)intSerializer.Deserialize(reader);
+                reader.ReadEndElement();
+
+                reader.MoveToContent();
+                ((SerializableDictionary<int, TrackNode>)this).ReadXml(reader);
+
+                reader.ReadEndElement();
+            }
+            reader.ReadEndElement();
+        }
+
+        public new void WriteXml(System.Xml.XmlWriter writer)
+        {
+            XmlSerializer intSerializer = new XmlSerializer(typeof(int));
+
+            writer.WriteStartElement(listName);
+
+            writer.WriteStartElement("objType");
+            intSerializer.Serialize(writer, objType);
+            writer.WriteEndElement();
+
+            ((SerializableDictionary<int, TrackNode>)this).WriteXml(writer);
+
+            writer.WriteEndElement();
+        }
+        #endregion
+    }
+
+    public class TrackList : SerializableDictionary<int, Track>, IXmlSerializable
+    {
+        public string videoFilePath;
+
+        protected override string listName
+        {
+            get { return "TrackList"; }
+        }
+        
+        protected override string itemName
+        {
+            get { return "TrackListItem"; }
+        }
+
+        protected override string keyName
+        {
+            get { return "TrackID"; }
+        }
+
+        protected override string valueName
+        {
+            get { return "Value"; }
+        }
+
+        #region IXmlSerializable Members
+
+        public new void ReadXml(XmlReader reader)
+        {
+            XmlSerializer stringSerializer = new XmlSerializer(typeof(string));
+
+            bool wasEmpty = reader.IsEmptyElement;
+            //reader.Read();
+
+            if (wasEmpty)
+                return;
+
+            reader.ReadStartElement(listName);
+
+            reader.ReadStartElement("videoFilePath");
+            videoFilePath = (string)stringSerializer.Deserialize(reader);
+            reader.ReadEndElement();
+
+            reader.MoveToContent();
+            ((SerializableDictionary<int, Track>)this).ReadXml(reader);
+
+            reader.ReadEndElement();
+        }
+
+        public new void WriteXml(System.Xml.XmlWriter writer)
+        {
+            XmlSerializer stringSerializer = new XmlSerializer(typeof(string));
+
+            writer.WriteStartElement(listName);
+
+            writer.WriteStartElement("videoFilePath");
+            stringSerializer.Serialize(writer, videoFilePath);
+            writer.WriteEndElement();
+
+            ((SerializableDictionary<int, Track>)this).WriteXml(writer);
+
+            writer.WriteEndElement();
+        }
+        #endregion
+    }
+}
