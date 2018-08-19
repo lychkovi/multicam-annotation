8c75b2bb4a25e5f0721c4cf1a3a044d70fe8efc0
lychkovi <lychkovi@gmail.com>
2018-08-19 16:49:59 +0300

    (VideoAnnotationAPP): Добавлена новая форма Form4, демонстрирующая возможность доступа к элементам управления формы через вспомогательный класс посредством ссылок на элементы управления.

---
+++ VideoAnnotationAPP/Form4.cs
@@ -0,0 +1,48 @@
+﻿// Form4.cs: Программа проверяет возможность доступа к элементам управления формы
+// через вспомогательные классы (по ссылкам на элементы управления). 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VideoAnnotationAPP
+{
+    public partial class Form4 : Form
+    {
+        Proxy proxy;
+
+        public Form4()
+        {
+            InitializeComponent();
+            proxy = new Proxy(textBox1, label1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            proxy.Update();
+        }
+    }
+
+
+    public class Proxy
+    {
+        protected TextBox textBox;
+        protected Label label;
+
+        public Proxy(TextBox textBoxIn, Label labelIn)
+        {
+            textBox = textBoxIn;
+            label = labelIn;
+        }
+
+        public void Update()
+        {
+            label.Text = textBox.Text;
+        }
+    }
+}
